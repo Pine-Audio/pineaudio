2a703a8e879531a9c14833c69c0048cd7d3ff288
Julian <julian.blazek@outlook.de>
2021-06-06 15:39:26 +0200

    Add tape to cassette and other component improvements

--- components/Cassette.vue
+++ components/Cassette.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="scene">
     <div class="case"></div>
-    <div class="label"></div>
+    <div class="label"><img src="https://source.unsplash.com/random" /></div>
     <div class="label-text">
       <div class="sticker">
         <div class="tape-description">
@@ -11,7 +11,13 @@
         <span><slot></slot></span>
       </div>
     </div>
-    <div class="band"></div>
+    <div class="band">
+      <div class="cover"></div>
+      <div id="left" class="bit" :style="rotationCSS"></div>
+      <div id="right" class="bit" :style="[rotationCSS]"></div>
+    </div>
+    <div id="left" class="tape" :style="sizeLeft"></div>
+    <div id="right" class="tape" :style="sizeRight"></div>
   </div>
 </template>
 
@@ -27,16 +33,45 @@ export default Vue.extend({
       type: String,
       default: 'stereo',
     },
+    rotation: {
+      type: Number,
+      default: 0,
+    },
+    completion: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    rotationCSS(): object {
+      return {
+        transform: 'rotate(-' + this.completion * 100 + 'deg)',
+      }
+    },
+    sizeLeft(): object {
+      const size: number = 200 - this.completion
+      return {
+        width: size + 'px',
+        height: size + 'px',
+      }
+    },
+    sizeRight(): object {
+      const size: number = 100 + this.completion
+      return {
+        width: size + 'px',
+        height: size + 'px',
+      }
+    },
   },
 })
 </script>
 
 <style>
 .scene {
-  width: 300px;
-  height: 200px;
+  width: 509px;
+  height: 320px;
   position: relative;
-  zoom: 1;
+  zoom: 2;
 }
 
 .case {
@@ -52,28 +87,40 @@ export default Vue.extend({
 }
 
 .label {
-  background-image: url('https://source.unsplash.com/random');
-  z-index: 0;
-  width: 95%;
-  height: 70%;
+  z-index: 1;
   position: absolute;
   top: 0;
-  margin: 3%;
-  background-repeat: no-repeat;
-  background-size: cover;
   filter: sepia(0.2) saturate(0.8) brightness(0.9);
+  overflow: hidden;
+  height: 100%;
+}
+
+.label img {
+  width: 100%;
+  object-fit: cover;
+  mask-size: 100%;
+  mask-image: url('~@/assets/cassette_mask_no_bits.png');
+  mask-position: 0px 0px;
 }
 
 .band {
-  background-image: url('~@/assets/cassette_overlay.png');
-  z-index: 1;
+  filter: brightness(0.2);
   width: 100%;
   height: 100%;
+  z-index: 3;
+  position: relative;
+}
+
+.cover {
+  background-image: url('~@/assets/cassette_overlay_no_bits.png');
+  z-index: 5;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
   position: absolute;
   background-repeat: no-repeat;
   background-size: contain;
-
-  filter: brightness(0.2);
 }
 
 .label-text {
@@ -119,5 +166,93 @@ export default Vue.extend({
 
 .tape-description ~ span {
   margin-left: 0.6em;
+}
+
+.bit {
+  background-image: url('~@/assets/cassette_bits.svg');
+  z-index: 3;
+  width: 49px;
+  height: 49px;
+  position: absolute;
+  transition: transform 0.3s;
+}
+
+.bit#left {
+  top: 37.5%;
+  left: 24.2%;
+}
+
+.bit#right {
+  top: 37.5%;
+  left: 66%;
+}
+
+.tape {
+  background: radial-gradient(
+      circle,
+      rgba(145, 145, 145, 0.2),
+      rgba(46, 46, 46, 0.2)
+    ),
+    radial-gradient(
+      circle,
+      transparent 30px,
+      rgb(29, 37, 43) 30px,
+      rgb(29, 37, 43) 10%,
+      rgb(11, 13, 15) 10%,
+      rgb(11, 13, 15) 11%,
+      rgb(29, 37, 43) 11%,
+      rgb(29, 37, 43) 20%,
+      rgb(11, 13, 15) 20%,
+      rgb(11, 13, 15) 21%,
+      rgb(29, 37, 43) 21%,
+      rgb(29, 37, 43) 30%,
+      rgb(11, 13, 15) 30%,
+      rgb(11, 13, 15) 31%,
+      rgb(29, 37, 43) 31%,
+      rgb(29, 37, 43) 40%,
+      rgb(11, 13, 15) 40%,
+      rgb(11, 13, 15) 41%,
+      rgb(29, 37, 43) 41%,
+      rgb(29, 37, 43) 50%,
+      rgb(11, 13, 15) 50%,
+      rgb(11, 13, 15) 51%,
+      rgb(29, 37, 43) 51%,
+      rgb(29, 37, 43) 60%,
+      rgb(11, 13, 15) 60%,
+      rgb(11, 13, 15) 61%,
+      rgb(29, 37, 43) 61%,
+      rgb(29, 37, 43) 70%,
+      rgb(11, 13, 15) 70%,
+      rgb(11, 13, 15) 71%,
+      rgb(29, 37, 43) 71%,
+      rgb(29, 37, 43) 80%,
+      rgb(11, 13, 15) 80%,
+      rgb(11, 13, 15) 81%,
+      rgb(29, 37, 43) 81%,
+      rgb(29, 37, 43) 90%,
+      rgb(11, 13, 15) 90%,
+      rgb(11, 13, 15) 91%,
+      rgb(29, 37, 43) 91%,
+      rgb(29, 37, 43) 100%
+    );
+  z-index: 0;
+  border-radius: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  transition: width 0.2s, height 0.2s;
+}
+
+.tape#left {
+  left: 29%;
+  top: 45.5%;
+  width: 200px;
+  height: 200px;
+}
+
+.tape#right {
+  left: 70.7%;
+  top: 45.5%;
+  width: 200px;
+  height: 200px;
 }
 </style>
--- pages/cassette.vue
+++ pages/cassette.vue
@@ -1,5 +1,16 @@
 <template>
-  <Casette tape-feature="MONO" tape-side="B">My cool Mixtape!!</Casette>
+  <div>
+    <Casette tape-feature="stereo" tape-side="B" :completion="completion"
+      >My cool Mixtape!!</Casette
+    >
+    <input
+      v-model.number="completion"
+      type="range"
+      min="0"
+      max="100"
+      value="0"
+    />
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,5 +18,15 @@ import Vue from 'vue'
 import Casette from '~/components/Cassette.vue'
 export default Vue.extend({
   components: { Casette },
+  data() {
+    return {
+      completion: 0 as number,
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.completion = this.completion + 0.1
+    }, 60)
+  },
 })
 </script>
